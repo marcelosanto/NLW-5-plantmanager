500d9e8082d7f37253869279dc13b3539ea8ea61
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-28 15:35:46 -0300

    add expo vector icons and expo google fonts

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,8 +5,9 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
-  Dimensions
+  Dimensions,
 } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 import colors from '../../styles/colors'
 import wateringImg from '../assets/watering.png'
@@ -18,19 +19,17 @@ export function Welcome() {
         Gerencie {'\n'} suas plantas {'\n'} de forma fácil
       </Text>
 
-      <Image 
-        source={wateringImg} 
-        style={styles.image} 
-        resizeMode='contain'
-      />
-      
+      <Image source={wateringImg} style={styles.image} resizeMode='contain' />
+
       <Text style={styles.subtitle}>
         Não esqueca mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
 
       <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-        <Text style={styles.buttonText}>></Text>
+        <Text>
+          <Feather name='chevron-right' style={styles.buttonIcon} />
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>
   )
@@ -56,7 +55,7 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    height: Dimensions.get('window').width * 0.7
+    height: Dimensions.get('window').width * 0.7,
   },
   button: {
     backgroundColor: colors.green,
@@ -67,8 +66,8 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56,
   },
-  buttonText: {
+  buttonIcon: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 32,
   },
 })
