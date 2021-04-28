103ee6c10cc8a3bc867d4fd50101b3beee4495fb
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-28 15:25:03 -0300

    codigo refatorado

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native'
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
 
 import colors from '../../styles/colors'
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button'
 
 export function Welcome() {
   return (
@@ -11,12 +17,21 @@ export function Welcome() {
       <Text style={styles.title}>
         Gerencie {'\n'} suas plantas {'\n'} de forma fácil
       </Text>
-      <Image source={wateringImg} style={styles.image} />
+
+      <Image 
+        source={wateringImg} 
+        style={styles.image} 
+        resizeMode='contain'
+      />
+      
       <Text style={styles.subtitle}>
         Não esqueca mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-      <Button title={'>'} onPress={() => ()} />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>></Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -25,7 +40,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -41,7 +56,19 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 294,
+    height: Dimensions.get('window').width * 0.7
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 20,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 })
